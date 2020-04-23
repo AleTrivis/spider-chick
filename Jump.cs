c69f612121a4212853988a9c54986edf017ae0a3
AleTrivis <64185188+AleTrivis@users.noreply.github.com>
2020-04-23 00:26:09 -0500

    Código para que salte el personaje

---
+++ Jump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    // Esta cosilla es para modificar el rango de la velocidad en el inspector.
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Al presionar el espacio
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Agrega velocidad para saltar.
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+}
